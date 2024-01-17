a52ad5904dc086a282dac8763ea3bc28e46e5525
Draken-101 <leonardofavion@gmail.com>
2024-01-17 12:44:24 -0600

    vistas

---
+++ js/scrips1.js
@@ -0,0 +1,24 @@
+// var bt1 = document.getElementById('1');
+// var bt2 = document.getElementById('2');
+// var bt3 = document.getElementById('3');
+// bt1.click();
+
+// function activateButtonAndFocus(button) {
+//     button.focus();
+//     button.click(); // Simula el clic en el botón
+//     console.log('El elemento con id ' + button.id + ' ha sido activado y tiene el foco.');
+// }
+
+// // Inicia la activación cada 5 segundos
+// setInterval(function () {
+//     activateButtonAndFocus(bt1);
+// }, 5000);
+
+// // Configura intervalos adicionales para los otros botones
+// setInterval(function () {
+//     activateButtonAndFocus(bt2);
+// }, 10000);  // 10 segundos
+
+// setInterval(function () {
+//     activateButtonAndFocus(bt3);
+// }, 15000);  // 15 segundos
